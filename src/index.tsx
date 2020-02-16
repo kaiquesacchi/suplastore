993a7e9c6fea8d153a6a4c8185438ca474b97d44
Kaique Sacchi <sacchikaique@gmail.com>
2020-02-16 18:06:32 -0300

    Added React Router Dom.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
